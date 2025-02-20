527c11127cc197828b59a1bbe2d8c49ebf7b4dd5
Ryan <ryantl66@gmail.com>
2025-02-20 18:32:51 -0500

    refactored

--- main.ino
+++ main.ino
@@ -1,8 +1,6 @@
-/* Ryan's Server Status */
 #include "DEV_Config.h"
 #include "EPD.h"
 #include "GUI_Paint.h"
-#include <stdlib.h>
 #include <WiFi.h>
 #include <ESP32Ping.h>
 #include <NTPClient.h>
@@ -11,76 +9,56 @@
 #include <Update.h>
 #include <ArduinoJson.h>
 
-#define FIRMWARE_URL1  "http://192.168.2.33/firmware_metadata.json"  // URL to JSON metadata file
-String current_version = "1.7";
+#define FIRMWARE_URL1 "http://192.168.2.33/firmware/firmware_metadata.json"  // URL to JSON metadata file
+String current_version = "1.0.2";
 
-// deep sleep
-#define uS_TO_S_FACTOR 1000000    /* Conversion factor for micro seconds to seconds */
-#define TIME_TO_SLEEP 1785        // sleep 30 minutes
+// Deep sleep settings
+#define uS_TO_S_FACTOR 1000000   /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP 1785       // sleep 29.5 minutes
 #define TIME_TO_SLEEP_NoWIFI 30  // sleep only 2 minutes if no wifi
 
-// Replace with your network credentials
+// Network credentials
 const char *ssid = "ssid";
 const char *password = "password";
 const char *MyHostName = "ESP32ServerMonitor";
 
-String firmware_url = "";
-String firmware_version = "";
-
-// Define NTP Client to get time
+// NTP Client settings
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP);
 
-//top bar title & IP string
-String titleIPString = "Ryan's Server Status      IP:";
-
-// Variables to save date and time
-String hour, minute, second;
-String Formatted_date, date, Time;
-
-//text and ip of servers
+// Server addresses
 IPAddress mediaHost(192, 168, 2, 2);
-const char *mediaTextOnline = "Media is ONLINE";
-const char *mediaTextOffline = "Media is OFFLINE";
 IPAddress fireHost(192, 168, 2, 102);
-const char *fireTextOnline = "pfsense is ONLINE";
-const char *fireTextOffline = "pfsense is OFFLINE";
 IPAddress unraidHost(192, 168, 2, 101);
-const char *unraidTextOnline = "UNRAID is ONLINE";
-const char *unraidTextOffline = "UNRAID is OFFLINE";
 IPAddress requestHost(192, 168, 2, 17);
-const char *requestTextOnline = "Request is ONLINE";
-const char *requestTextOffline = "Request is OFFLINE";
 IPAddress vpnHost(192, 168, 2, 99);
-const char *vpnTextOnline = "VPN Servers are ONLINE";
-const char *vpnTextOffline = "VPN Servers are OFFLINE";
 IPAddress vmHost(192, 168, 2, 250);
-const char *vmTextOnline = "VMs are ONLINE";
-const char *vmTextOffline = "VMs are OFFLINE";
 
+// Server statuses
 bool pingMedia, pingFire, pingUnraid, pingVPN, pingVM, pingRequest;
 
-/* Entry point ----------------------------------------------------------------*/
 void setup() {
-  //Serial.begin(115200);
+  delay(500);
+  Serial.begin(115200);
   wifiInit();
   delay(5000);
   checkForFirmwareUpdate();
-  analogRead(33);  //esp32 pin 33 battery voltage stepped down from 4.2v to 3.2v
+  analogRead(33);  // Battery voltage read
+  delay(500);
+  int batteryLevel = map(analogRead(33), 3412.0f, 4095.0f, 0, 100);
+  String batLevel = String(batteryLevel) + "%";
   ePaperInit();
 
-#if 1
-  if (WiFi.status() != WL_CONNECTED) {
-    esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP_NoWIFI * uS_TO_S_FACTOR);
-    drawNoConnection();
-  } else {
+  if (WiFi.status() == WL_CONNECTED) {
     esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
     pingServers();
-    drawMainServerStatus();
+    drawMainServerStatus(batteryLevel, batLevel);
+  } else {
+    esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP_NoWIFI * uS_TO_S_FACTOR);
+    drawBatteryAndNoWifi(batteryLevel, batLevel);
   }
 
-#endif
-  //finally sleep
+  // Sleep after display
   Serial.println("Going to sleep now");
   delay(500);
   Serial.flush();
@@ -88,16 +66,18 @@ void setup() {
 }
 
 
-void wifiInit() {  //init wifi
+//function refactoring
+void wifiInit() {
   WiFi.setHostname(MyHostName);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  delay(500);
-  //if (WiFi.status() == WL_CONNECTED) {
+  //while (WiFi.status() != WL_CONNECTED) {
+  // delay(500);
+  // Serial.print(".");
   //}
 }
 
-void ePaperInit() {  // init the epaper display and clear it
+void ePaperInit() {
   if (DEV_Module_Init() != 0) {
     while (1)
       ;
@@ -107,149 +87,140 @@ void ePaperInit() {  // init the epaper display and clear it
   DEV_Delay_ms(500);
 }
 
-void drawNoConnection() { //draw no connection
-  //Create a new image cache named IMAGE_BW and fill it with white
-  UBYTE *BlackImage, *RYImage;  // Red or Yellow
+
+void drawBatteryAndNoWifi(int batteryLevel, String batLevel) {
+  UBYTE *BlackImage, *RYImage;
   UWORD Imagesize = ((EPD_2IN9B_V4_WIDTH % 8 == 0) ? (EPD_2IN9B_V4_WIDTH / 8) : (EPD_2IN9B_V4_WIDTH / 8 + 1)) * EPD_2IN9B_V4_HEIGHT;
-  if ((BlackImage = (UBYTE *)malloc(Imagesize)) == NULL) {
-    printf("Failed to apply for black memory...\r\n");
-    while (1)
-      ;
-  }
-  if ((RYImage = (UBYTE *)malloc(Imagesize)) == NULL) {
-    printf("Failed to apply for red memory...\r\n");
-    while (1)
-      ;
-  }
+  allocateMemoryForImages(&BlackImage, &RYImage, Imagesize);
+
   Paint_NewImage(BlackImage, EPD_2IN9B_V4_WIDTH, EPD_2IN9B_V4_HEIGHT, 270, WHITE);
   Paint_NewImage(RYImage, EPD_2IN9B_V4_WIDTH, EPD_2IN9B_V4_HEIGHT, 270, WHITE);
-  //draw black image
+
+  // *----------- Draw black image ------------*
   Paint_SelectImage(BlackImage);
   Paint_Clear(WHITE);
+  drawBatteryBlack(batteryLevel, batLevel);
   Paint_DrawRectangle(7, 17, 290, 113, BLACK, DOT_PIXEL_1X1, DRAW_FILL_EMPTY);
-  //battery percentage readout
-  int batteryLevel = map(analogRead(33), 3412, 4095, 0, 100);
-  String batLevel = String(batteryLevel) + "%";
-  //draw empty battery battery icon
-  Paint_DrawRectangle(7, 117, 25, 125, BLACK, DOT_PIXEL_1X1, DRAW_FILL_EMPTY);
-  Paint_DrawRectangle(25, 120, 27, 123, BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);
-  if (batteryLevel > 20) {
-    Paint_DrawString_EN(28, 116, batLevel.c_str(), &Font12, WHITE, BLACK);  //battery percentage readout black
-  }
-  if (batteryLevel >= 90) {
-    Paint_DrawRectangle(7, 117, 25, 125, BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);  //90-100%
-  } else if (batteryLevel >= 80) {
-    Paint_DrawRectangle(7, 117, 22, 125, BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);  //80-90%
-  } else if (batteryLevel >= 65) {
-    Paint_DrawRectangle(7, 117, 20, 125, BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);  //65-80%
-  } else if (batteryLevel >= 50) {
-    Paint_DrawRectangle(7, 117, 18, 125, BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);  //50-65%
-  } else if (batteryLevel >= 40) {
-    Paint_DrawRectangle(7, 117, 16, 125, BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);  //40-50%
-  } else if (batteryLevel >= 30) {
-    Paint_DrawRectangle(7, 117, 14, 125, BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);  //30-40%
-  } else if (batteryLevel > 20) {
-    Paint_DrawRectangle(7, 117, 12, 125, BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);  //20-30%
-  }
-  //draw red strings or shapes
-  //2.Draw red image
+
+  // *----------- Draw red image ------------*
   Paint_SelectImage(RYImage);
   Paint_Clear(WHITE);
 
-  titleIPString = titleIPString + "Not connected";
+  String titleIPString = "Not connected";
   Paint_DrawString_EN(0, 0, titleIPString.c_str(), &Font12, BLACK, WHITE);
 
   Paint_DrawString_EN(10, 50, "WiFi connection is not", &Font16, BLACK, WHITE);
   Paint_DrawString_EN(10, 65, "available", &Font16, BLACK, WHITE);
-  if (batteryLevel > 10 && batteryLevel <= 20) {
-    Paint_DrawString_EN(28, 116, batLevel.c_str(), &Font12, WHITE, BLACK);       //battery percentage readout red
-    Paint_DrawRectangle(7, 117, 10, 125, BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);  // 10-20%
-  } else if (batteryLevel <= 10) {
-    Paint_DrawString_EN(28, 116, batLevel.c_str(), &Font12, WHITE, BLACK);      //battery percentage readout red
-    Paint_DrawRectangle(7, 117, 8, 125, BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);  // less than 10%
-  }
-
+  drawBatteryRed(batteryLevel, batLevel);
   EPD_2IN9B_V4_Display_Base(BlackImage, RYImage);
   //EPD_2IN9B_V4_Display(BlackImage, RYImage);
   DEV_Delay_ms(2000);
   EPD_2IN9B_V4_Sleep();
   free(BlackImage);
   free(RYImage);
-  BlackImage = NULL;
-  RYImage = NULL;
   DEV_Delay_ms(2000);  //important, at least 2s
-  // close 5V
 }
 
-void drawMainServerStatus() {  //draw strings or shapes
-  //Create a new image cache named IMAGE_BW and fill it with white
-  UBYTE *BlackImage, *RYImage;  // Red or Yellow
+void allocateMemoryForImages(UBYTE **BlackImage, UBYTE **RYImage, UWORD Imagesize) {
+  *BlackImage = (UBYTE *)malloc(Imagesize);
+  *RYImage = (UBYTE *)malloc(Imagesize);
+  if (*BlackImage == NULL || *RYImage == NULL) {
+    Serial.println("Memory allocation failed");
+    while (1)
+      ;
+  }
+}
+
+void drawMainServerStatus(int batteryLevel, String batLevel) {
+  UBYTE *BlackImage, *RYImage;
   UWORD Imagesize = ((EPD_2IN9B_V4_WIDTH % 8 == 0) ? (EPD_2IN9B_V4_WIDTH / 8) : (EPD_2IN9B_V4_WIDTH / 8 + 1)) * EPD_2IN9B_V4_HEIGHT;
-  if ((BlackImage = (UBYTE *)malloc(Imagesize)) == NULL) {
-    printf("Failed to apply for black memory...\r\n");
-    while (1)
-      ;
-  }
-  if ((RYImage = (UBYTE *)malloc(Imagesize)) == NULL) {
-    printf("Failed to apply for red memory...\r\n");
-    while (1)
-      ;
-  }
+  allocateMemoryForImages(&BlackImage, &RYImage, Imagesize);
+  const char *serverText[] = {
+    pingFire ? "pfsense is ONLINE" : "pfsense is OFFLINE",
+    pingUnraid ? "UNRAID is ONLINE" : "UNRAID is OFFLINE",
+    pingMedia ? "Media is ONLINE" : "OASIS Media is OFFLINE",
+    pingRequest ? "Request is ONLINE" : "OASIS Request is OFFLINE",
+    pingVPN ? "VPN Servers are ONLINE" : "VPN Servers are OFFLINE",
+    pingVM ? "VMs are ONLINE" : "VMs are OFFLINE"
+  };
+
   Paint_NewImage(BlackImage, EPD_2IN9B_V4_WIDTH, EPD_2IN9B_V4_HEIGHT, 270, WHITE);
   Paint_NewImage(RYImage, EPD_2IN9B_V4_WIDTH, EPD_2IN9B_V4_HEIGHT, 270, WHITE);
-
-  //draw black image
+  // *----------- Draw black image ------------*
   Paint_SelectImage(BlackImage);
   Paint_Clear(WHITE);
 
+  //draw box
+  Paint_DrawRectangle(7, 17, 290, 113, BLACK, DOT_PIXEL_1X1, DRAW_FILL_EMPTY);
 
-  if (pingFire == true) {
-    Paint_DrawString_EN(10, 20, fireTextOnline, &Font16, WHITE, BLACK);
-  }
-  if (pingUnraid == true) {
-    Paint_DrawString_EN(10, 35, unraidTextOnline, &Font16, WHITE, BLACK);
-  }
-  if (pingMedia == true) {
-    Paint_DrawString_EN(10, 50, mediaTextOnline, &Font16, WHITE, BLACK);
-  }
-  if (pingRequest == true) {
-    Paint_DrawString_EN(10, 65, requestTextOnline, &Font16, WHITE, BLACK);
-  }
-  if (pingVPN == true) {
-    Paint_DrawString_EN(10, 80, vpnTextOnline, &Font16, WHITE, BLACK);
-  }
-  if (pingVM == true) {
-    Paint_DrawString_EN(10, 95, vmTextOnline, &Font16, WHITE, BLACK);
-  }
+  // Draw online server status
+  drawServerStatusONLINE(serverText);
 
-  // Extract datetime
-  // Initialize a NTPClient to get time
+  // Draw time and battery in black
+  drawBatteryBlack(batteryLevel, batLevel);
+  drawTimeDate();
+
+
+  // *----------- Draw red image ------------*
+  Paint_SelectImage(RYImage);
+  Paint_Clear(WHITE);
+
+  // Draw offline server status
+  drawServerStatusOFFLINE(serverText);
+
+  //draw red battery level
+  drawBatteryRed(batteryLevel, batLevel);
+
+  //draw red wifi ip top
+  Paint_DrawString_EN(0, 0, WiFi.localIP().toString().c_str(), &Font12, BLACK, WHITE);
+
+  EPD_2IN9B_V4_Display_Base(BlackImage, RYImage);
+  free(BlackImage);
+  free(RYImage);
+  DEV_Delay_ms(2000);
+  EPD_2IN9B_V4_Sleep();
+}
+
+void drawServerStatusONLINE(const char *serverText[]) {
+  for (int i = 0; i < 6; i++) {
+    if (String(serverText[i]).indexOf("ONLINE") > 0) {
+      Paint_DrawString_EN(10, 20 + i * 15, serverText[i], &Font16, WHITE, BLACK);
+    }
+  }
+}
+
+void drawServerStatusOFFLINE(const char *serverText[]) {
+  for (int i = 0; i < 6; i++) {
+    if (String(serverText[i]).indexOf("OFFLINE") > 0) {
+      Paint_DrawString_EN(10, 20 + i * 15, serverText[i], &Font16, BLACK, WHITE);
+    }
+  }
+}
+
+void drawTimeDate() {
   timeClient.begin();
   timeClient.setTimeOffset(-18000);
   while (!timeClient.update()) {
     timeClient.forceUpdate();
   }
-  Formatted_date = timeClient.getFormattedDate();
-  hour = Formatted_date.substring(11, 13);
-  minute = Formatted_date.substring(14, 16);
-  second = Formatted_date.substring(17, 19);
+  String Formatted_date = timeClient.getFormattedDate();
+  String hour = Formatted_date.substring(11, 13);
+  String minute = Formatted_date.substring(14, 16);
+  String second = Formatted_date.substring(17, 19);
   if ((hour.toInt() == 0) or (hour.toInt() == 12)) {  // Use 12 instead of 0
     hour = "12";
   } else {
     hour = hour.toInt() % 12;  // Use String
   }
-  Time = hour + ":" + minute + ":" + second;
+  String Time = hour + ":" + minute + ":" + second;
   int splitT = Formatted_date.indexOf("T");
-  date = Formatted_date.substring(0, splitT);
-  Paint_DrawRectangle(7, 17, 290, 113, BLACK, DOT_PIXEL_1X1, DRAW_FILL_EMPTY);
+  String date = Formatted_date.substring(0, splitT);
   String dateTime = date + " " + Time;
   Paint_DrawString_EN(159, 115, dateTime.c_str(), &Font12, BLACK, WHITE);
+}
 
-  //battery percentage readout
-  int batteryLevel = map(analogRead(33), 3412, 4095, 0, 100);
-  String batLevel = String(batteryLevel) + "%";
-
-  //draw empty battery battery icon
+void drawBatteryBlack(int batteryLevel, String batLevel) {
+  //draw black empty battery icon
   Paint_DrawRectangle(7, 117, 25, 125, BLACK, DOT_PIXEL_1X1, DRAW_FILL_EMPTY);
   Paint_DrawRectangle(25, 120, 27, 123, BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);
 
@@ -272,129 +243,55 @@ void drawMainServerStatus() {  //draw strings or shapes
   } else if (batteryLevel > 20) {
     Paint_DrawRectangle(7, 117, 12, 125, BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);  //20-30%
   }
-
-
-  //draw red strings or shapes
-  //2.Draw red image
-  Paint_SelectImage(RYImage);
-  Paint_Clear(WHITE);
-
-  titleIPString = titleIPString + WiFi.localIP().toString();
-  Paint_DrawString_EN(0, 0, titleIPString.c_str(), &Font12, BLACK, WHITE);
-
-  if (pingFire == false) {
-    Paint_DrawString_EN(10, 20, fireTextOffline, &Font16, BLACK, WHITE);
-  }
-  if (pingUnraid == false) {
-    Paint_DrawString_EN(10, 35, unraidTextOffline, &Font16, BLACK, WHITE);
-  }
-  if (pingMedia == false) {
-    Paint_DrawString_EN(10, 50, mediaTextOffline, &Font16, BLACK, WHITE);
-  }
-  if (pingRequest == false) {
-    Paint_DrawString_EN(10, 65, requestTextOffline, &Font16, BLACK, WHITE);
-  }
-  if (pingVPN == false) {
-    Paint_DrawString_EN(10, 80, vpnTextOffline, &Font16, BLACK, WHITE);
-  }
-  if (pingVM == false) {
-    Paint_DrawString_EN(10, 95, vmTextOffline, &Font16, BLACK, WHITE);
-  }
-
-  if (batteryLevel > 10 && batteryLevel <= 20) {
-    Paint_DrawString_EN(28, 116, batLevel.c_str(), &Font12, WHITE, BLACK);       //battery percentage readout red
-    Paint_DrawRectangle(7, 117, 10, 125, BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);  // 10-20%
-  } else if (batteryLevel <= 10) {
-    Paint_DrawString_EN(28, 116, batLevel.c_str(), &Font12, WHITE, BLACK);      //battery percentage readout red
-    Paint_DrawRectangle(7, 117, 8, 125, BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);  // less than 10%
-  }
-
-  EPD_2IN9B_V4_Display_Base(BlackImage, RYImage);
-  //EPD_2IN9B_V4_Display(BlackImage, RYImage);
-  DEV_Delay_ms(2000);
-
-  EPD_2IN9B_V4_Sleep();
-  free(BlackImage);
-  free(RYImage);
-  BlackImage = NULL;
-  RYImage = NULL;
-  DEV_Delay_ms(2000);  //important, at least 2s
-  // close 5V
 }
 
-void pingServers() {  //ping the servers once multiple times if failed to speed up results
-  delay(500);
-  pingFire = Ping.ping(fireHost, 1);
-  if (pingFire == false) {
-    pingFire = Ping.ping(fireHost, 1);
-    if (pingFire == false) {
-      pingFire = Ping.ping(fireHost, 2);
-    }
+void drawBatteryRed(int batteryLevel, String batLevel) {
+  if (batteryLevel <= 20) {
+    Paint_DrawString_EN(28, 116, batLevel.c_str(), &Font12, WHITE, BLACK);  //battery percentage readout red
   }
-  delay(500);
-  pingUnraid = Ping.ping(unraidHost, 1);
-  if (pingUnraid == false) {
-    pingUnraid = Ping.ping(unraidHost, 1);
-    if (pingUnraid == false) {
-      pingUnraid = Ping.ping(unraidHost, 2);
-    }
+  if (batteryLevel > 10 && batteryLevel <= 20) {
+    Paint_DrawRectangle(7, 117, 10, 125, BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);  // 10-20%
+  } else if (batteryLevel <= 10) {
+    Paint_DrawRectangle(7, 117, 8, 125, BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);  // less than 10%
   }
-  delay(500);
-  pingMedia = Ping.ping(mediaHost, 1);
-  if (pingMedia == false) {
-    pingMedia = Ping.ping(mediaHost, 1);
-    if (pingMedia == false) {
-      pingMedia = Ping.ping(mediaHost, 2);
-    }
-  }
-  delay(500);
-  pingRequest = Ping.ping(requestHost, 1);
-  if (pingRequest == false) {
-    pingRequest = Ping.ping(requestHost, 1);
-    if (pingRequest == false) {
-      pingRequest = Ping.ping(requestHost, 2);
-    }
-  }
-  delay(500);
-  pingVPN = Ping.ping(vpnHost, 1);
-  if (pingVPN == false) {
-    pingVPN = Ping.ping(vpnHost, 1);
-    if (pingVPN == false) {
-      pingVPN = Ping.ping(vpnHost, 2);
-    }
-  }
-  delay(500);
-  pingVM = Ping.ping(vmHost, 1);
-  if (pingVM == false) {
-    pingVM = Ping.ping(vmHost, 1);
-    if (pingVM == false) {
-      pingVM = Ping.ping(vmHost, 2);
-    }
+}
+
+void pingServers() {
+  pingFire = pingServer(fireHost);
+  pingUnraid = pingServer(unraidHost);
+  pingMedia = pingServer(mediaHost);
+  pingRequest = pingServer(requestHost);
+  pingVPN = pingServer(vpnHost);
+  pingVM = pingServer(vmHost);
+}
+
+bool pingServer(IPAddress server) {
+  Serial.println("Pinging server...");
+
+  // Using the ping function directly, it returns a boolean
+  bool isReachable = Ping.ping(server, 2);  // 2 retries
+
+  if (isReachable) {
+    Serial.println("Server is online");
+    return true;
+  } else {
+    Serial.println("Server is offline");
+    return false;
   }
 }
 
 void checkForFirmwareUpdate() {
   HTTPClient http;
   http.begin(FIRMWARE_URL1);
-  // Make an HTTP request to get the firmware metadata JSON
   int httpCode = http.GET();
-  if (httpCode == 200) {
+  if (httpCode == HTTP_CODE_OK) {
     String payload = http.getString();
-    Serial.println("Metadata JSON received:");
-
-    // Parse JSON
     DynamicJsonDocument doc(1024);
     deserializeJson(doc, payload);
-    firmware_version = doc["version"].as<String>();
-    firmware_url = doc["url"].as<String>();
-
-    Serial.println("Firmware Version: " + firmware_version);
-    Serial.println("Firmware URL: " + firmware_url);
-
-    // Check if the firmware version is newer than the current one
-    if (isNewVersionAvailable(firmware_version)) {
-      Serial.println("New firmware available. Starting update...");
-
+    String firmware_version = doc["version"].as<String>();
+    String firmware_url = doc["url"].as<String>();
+    if (firmware_version != current_version && firmware_version.toInt() > current_version.toInt()) {
+      Serial.println("Updating firmware...");
       // Start the firmware update process
       performOTAUpdate(firmware_url);
     } else {
@@ -403,23 +300,14 @@ void checkForFirmwareUpdate() {
   } else {
     Serial.println("Failed to fetch metadata from server. HTTP Code: " + String(httpCode));
   }
-
   http.end();
-}
-
-bool isNewVersionAvailable(const String &new_version) {
-  // Compare the current firmware version with the new version
-  // You can replace this with a more complex version comparison logic if needed
-    // Get the current firmware version (you can use other methods)
-  
-  return (new_version != current_version);
 }
 
 void performOTAUpdate(const String &url) {
   WiFiClient client;
   HTTPClient http;
   http.begin(client, url);
-  
+
   // Get firmware file
   int httpCode = http.GET();
   if (httpCode == 200) {
@@ -458,6 +346,6 @@ void performOTAUpdate(const String &url) {
   http.end();
 }
 
-/* The main loop -------------------------------------------------------------*/
 void loop() {
+  // Your loop code goes here.
 }
